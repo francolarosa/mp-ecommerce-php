0c1d58d70a09c562320793f7a6e46ca1df8d4d60
francolarosa <franco.hugo.larosa@gmail.com>
2020-08-21 18:53:04 -0300

    Se relizaron algunos cambios

--- backend/notification/index.php
+++ backend/notification/index.php
@@ -1,10 +1,28 @@
 <?php
 
-// $access_token = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
-$json = file_get_contents('php://input', true);
+    require '../../vendor/autoload.php';
 
-$registro_de_pagos = fopen("pagos.json", "w");
 
-fwrite($registro_de_pagos,$json . '\n');
+    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+    $merchant_order = null;
+
+    switch($_GET["topic"]) {
+        case "payment":
+            $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
+            // Get the payment and the corresponding merchant_order reported by the IPN.
+            $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
+
+            $json_file = fopen("pago.json","w");
+            fwrite($json_file,json_encode($payment));
+            fwrite($json_file,json_encode($merchant_order));
+            break;
+        case "merchant_order":
+            $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
+            $json_file = fopen("pago.json","w");
+            fwrite($json_file,json_encode($merchant_order));
+            break;
+    }
+
 
 ?>
